22957080a8aab073c23a74a50a459f8c9d7960db
Semra İnce <semrance9@gmail.com>
2021-06-02 20:44:24 +0300

    Initial Commit

---
+++ TwitterClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwitterClone
+//
+//  Created by semra on 2.06.2021.
+//  Copyright © 2021 semra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
